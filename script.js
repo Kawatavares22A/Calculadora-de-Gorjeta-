2f995deb743d342d75a0b78a2605b5a4e86644f7
Kawã Tavares <kawa556622@gmai.com>
2024-03-31 17:13:02 -0300

    agora sim acabou

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ let conta = 0
 let pessoas = 0
 let porcentagem = 0
 
-
 const contaInput = document.querySelector("#conta")
 contaInput.addEventListener("input", receberValorConta)
 
@@ -11,14 +10,15 @@ function receberValorConta(evento) {
   conta = Number(evento.target.value)
   calcular()
 }
-  const pessoasInput = document.querySelector("#pessoas")
-  pessoasInput.addEventListener("input", receberQuantidadePessoas)
 
+
+const pessoasInput = document.querySelector("#pessoas")
+pessoasInput.addEventListener("input", receberQuantidadePessoas)
 
 
 function receberQuantidadePessoas(evento) {
   const paragrafoErro = document.querySelector(".pessoas #erro")
-  const divErro = document.querySelector(".pessoas input-box")
+  const divErro = document.querySelector(".pessoas .input-box")
 
 
   if (evento.target.value === "0") {
@@ -33,17 +33,17 @@ function receberQuantidadePessoas(evento) {
 }
 
 
-  const botoesGorjeta = document.querySelectorAll(".gorjeta input[type='button']")
+const botoesGorjeta = document.querySelectorAll(".gorjeta input[type='button']")
 botoesGorjeta.forEach(botao => {
   botao.addEventListener("click", receberPorcentagem)
 })
 
 
 function receberPorcentagem(evento) {
-  
+
   botoesGorjeta.forEach(botao => {
     botao.classList.remove("botao-ativo")
-   
+
     if (botao.value === evento.target.value) {
       botao.classList.add("botao-ativo")
     }
